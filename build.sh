e09b83acb95aa5a5ee1bdd4698d9e7c0c86d0182
dave <davemollen@gmail.com>
2023-01-15 23:59:01 +0100

    Disable cache when building docker image

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-DOCKER_BUILDKIT=1 docker build --rm --file docker/modduo/Dockerfile --output out/modduo . && \
-DOCKER_BUILDKIT=1 docker build --rm --file docker/moddwarf/Dockerfile --output out/moddwarf .
+DOCKER_BUILDKIT=1 docker build --no-cache --file docker/modduo/Dockerfile --output out/modduo . && \
+DOCKER_BUILDKIT=1 docker build --no-cache --file docker/moddwarf/Dockerfile --output out/moddwarf .
