6e8af270fc3b9db53e4a9cc5d9db4a930ffeeebf
dave <davemollen@gmail.com>
2023-05-08 22:59:50 +0200

    Set rust toolchain to 1.58.1

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-DOCKER_BUILDKIT=1 docker build --no-cache --file docker/modduo/Dockerfile --output out/modduo . && \
-DOCKER_BUILDKIT=1 docker build --no-cache --file docker/moddwarf/Dockerfile --output out/moddwarf .
+DOCKER_BUILDKIT=1 docker build --file docker/modduo/Dockerfile --output out/modduo . && \
+DOCKER_BUILDKIT=1 docker build --file docker/moddwarf/Dockerfile --output out/moddwarf .
+docker builder prune --force
