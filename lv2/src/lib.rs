d266176fb484ac223b9b586aa7d611e7bbd348df
Dave Mollen <dave.mollen@infi.nl>
2024-06-07 14:20:11 +0200

    Fix build

--- lv2/src/lib.rs
+++ lv2/src/lib.rs
@@ -35,8 +35,8 @@ impl Plugin for DmOctaver {
   // Process a chunk of audio. The audio ports are dereferenced to slices, which the plugin
   // iterates over.
   fn run(&mut self, ports: &mut Ports, _features: &mut (), _sample_count: u32) {
-    let threshold = *ports.threshold.dbtoa();
-    let gain = *ports.gain.dbtoa();
+    let threshold = (*ports.threshold).dbtoa();
+    let gain = (*ports.gain).dbtoa();
     let mix = *ports.mix * 0.01;
 
     for (in_frame, out_frame) in ports.input.iter().zip(ports.output.iter_mut()) {
